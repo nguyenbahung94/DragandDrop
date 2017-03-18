5414f48fe7b360318de5d37013e5dd561ce162f8
Tung Phan Thanh <thisisanewstyle@gmail.com>
2017-03-18 12:15:02 +0700

    Doing

--- TestDragAndDrop/app/src/main/java/hung/testdraganddrop/MainActivity.java
+++ TestDragAndDrop/app/src/main/java/hung/testdraganddrop/MainActivity.java
@@ -1,13 +1,82 @@
 package hung.testdraganddrop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imgattach;
+    private String pictureImagePath = "";
+    private static int SELECTED = 0;
+    private LinearLayout attachments;
+    private ArrayList<String> listImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imgattach = (ImageView) findViewById(R.id.attach);
+        attachments = (LinearLayout) findViewById(R.id.attachmentss);
+        imgattach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openBackCamera();
+                imgattach.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void openBackCamera() {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = timeStamp + ".jpg";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        pictureImagePath = storageDir.getAbsolutePath() + "/" + imageFileName;
+        File file = new File(pictureImagePath);
+        Uri outputFileUri = Uri.fromFile(file);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+        startActivityForResult(cameraIntent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 1) {
+            galleryAddPic();
+            File imgFile = new File(pictureImagePath);
+            if (imgFile.exists()) {
+                Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                listImage.add(imgFile.getAbsolutePath());
+                LayoutInflater inflater = getLayoutInflater();
+                View myLayout = inflater.inflate(R.layout.activity_show_a_picture, attachments, false);
+                ImageView imagetam = (ImageView) myLayout.findViewById(R.id.image_show_a_picture);
+                imagetam.setImageBitmap(myBitmap);
+                attachments.addView(myLayout);
+                imgattach.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(pictureImagePath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        sendBroadcast(mediaScanIntent);
     }
 }
